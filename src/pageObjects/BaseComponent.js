1d0be6733a0cfe70aa3fd646dd9c5609b18471c8
staran <staran@griddynamics.com>
2025-11-29 12:27:46 +0200

    feat: add example method

--- src/pageObjects/BaseComponent.js
+++ src/pageObjects/BaseComponent.js
@@ -1,5 +1,11 @@
 
 export default class BaseComponent {
-    constructor(page) {
+    constructor(page, container) {
+        this.page = page;
+        this.container = container ?? page.locator('body')
+    }
+
+    async waitLoaded() {
+        await this.container.waitFor({state: 'visible'})
     }
 }
