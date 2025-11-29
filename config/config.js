d8e08abb7777f3378d40fa6803211bd8b3ff8922
staran <staran@griddynamics.com>
2025-11-29 12:22:43 +0200

    feat: env variables and CLI

---
+++ config/config.js
@@ -0,0 +1,13 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const config =  {
+    baseURL: process.env.BASE_URL,
+    baseURLStage: process.env.BASE_URL_STAGE,
+    httpCredentials: {
+        username: process.env.HTTP_CREDENTIALS_USERNAME,
+        password: process.env.HTTP_CREDENTIALS_PASSWORD,
+    },
+}
+
+export default config;
